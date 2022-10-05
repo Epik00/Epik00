c74737dbc157ebcdba00dfe233b331460d375bc2
Epik00 <114663528+Epik00@users.noreply.github.com>
2022-10-05 14:44:13 +0200

    d

--- comandos/epik00.sh
+++ comandos/epik00.sh
@@ -21,14 +21,14 @@ while [[ $x = x ]]; do
    name_epop=$(top -b -n 1 | grep epop.sh | head -n1 | rev | awk '{print $1}' | rev | tr -d "[:space:]")
    start_epop=$(cat "$Dir_config" | grep startepop= | awk '{print $2}' | tr -d "[:space:]")
    if [[ $name_epop = "epop.sh" ]]; then
-      status_epop_m="Activo"
-      status_epop=$(echo -e "$verde" "Activo" "$normal")
+      status_epop_m="  Activo"
+      status_epop=$(echo -e "$verde" "  Activo" "$normal")
    else
-      status_epop_m="Inactivo"
-      status_epop=$(echo -e "$rojo" "Inactivo" "$normal")
+      status_epop_m="InActivo"
+      status_epop=$(echo -e "$rojo" "InActivo" "$normal")
    fi
    if [[ $start_epop = "true" ]]; then
-      start_epop_h=$(echo -e "$verde" "Activado" "$normal")
+      start_epop_h=$(echo -e "$verde" "Activado   " "$normal")
    else
       start_epop_h=$(echo -e "$rojo" "Desactivado" "$normal")
    fi
@@ -41,14 +41,14 @@ while [[ $x = x ]]; do
    name_hist=$(top -b -n 1 | grep hist.sh | head -n1 | rev | awk '{print $1}' | rev | tr -d "[:space:]")
    start_hist=$(cat "$Dir_config" | grep starthistorial= | awk '{print $2}' | tr -d "[:space:]")
    if [[ $name_hist = "hist.sh" ]]; then
-      status_hist=$(echo -e "$verde" "Activo" "$normal")
-      status_hist_m="Activo"
+      status_hist=$(echo -e "$verde" "  Activo" "$normal")
+      status_hist_m="  Activo"
    else
-      status_hist_m="Inactivo"
-      status_hist=$(echo -e "$rojo" "Inactivo" "$normal")
+      status_hist_m="InActivo"
+      status_hist=$(echo -e "$rojo" "InActivo" "$normal")
    fi
    if [[ $start_hist = "true" ]]; then
-      start_hist_h=$(echo -e "$verde" "Activado" "$normal")
+      start_hist_h=$(echo -e "$verde" "Activado   " "$normal")
    else
       start_hist_h=$(echo -e "$rojo" "Desactivado" "$normal")
    fi
@@ -60,14 +60,14 @@ while [[ $x = x ]]; do
    name_act=$(top -b -n 1 | grep act.sh | head -n1 | rev | awk '{print $1}' | rev | tr -d "[:space:]")
    start_act=$(cat "$Dir_config" | grep act00= | awk '{print $2}' | tr -d "[:space:]")
    if [[ $name_act = "act.sh" ]]; then
-      status_act=$(echo -e "$verde" "Activo" "$normal")
-      status_act_m="Activo"
+      status_act=$(echo -e "$verde" "  Activo" "$normal")
+      status_act_m="  Activo"
    else
-      status_act=$(echo -e "$rojo" "Inactivo" "$normal")
-      status_act_m="Inactivo"
+      status_act=$(echo -e "$rojo" "InActivo" "$normal")
+      status_act_m="InActivo"
    fi
    if [[ $start_act = "true" ]]; then
-      start_act_h=$(echo -e "$verde" "Activado" "$normal")
+      start_act_h=$(echo -e "$verde" "Activado   " "$normal")
    else
       start_act_h=$(echo -e "$rojo" "Desactivado" "$normal")
    fi
@@ -83,22 +83,22 @@ while [[ $x = x ]]; do
    echo -e "$azul" "  #       #       #    #  #    #   #   #   #" "$normal"
    echo -e "$azul" "  #####   #       #    #   #    ###     ###" "$normal"
    echo
-   echo " Num   Procesos    Estado     Pid        Inicio"
+   echo " Num   Procesos   Estado      Inicio           Pid"
    echo
-   printf "  1)   epop.sh    "
-   printf "$status_epop    "
-   printf "$pid_epop    "
-   printf "$start_epop_h"
+   printf "  1)   epop.sh  "
+   printf "$status_epop  "
+   printf "$start_epop_h     "
+   printf "$pid_epop"
    echo
-   printf "  2)   hist.sh    "
-   printf "$status_hist    "
-   printf "$pid_hist    "
-   printf "$start_hist_h"
+   printf "  2)   hist.sh  "
+   printf "$status_hist  "
+   printf "$start_hist_h     "
+   printf "$pid_hist"
    echo
-   printf "  3)   act.sh     "
-   printf "$status_act    "
-   printf "$pid_act    "
-   printf "$start_act_h"
+   printf "  3)   act.sh   "
+   printf "$status_act  "
+   printf "$start_act_h     "
+   printf "$pid_act"
    echo
    printf "  0)   Salir"
    #Input
@@ -116,7 +116,7 @@ while [[ $x = x ]]; do
    #Epop
    if [[ $menu_principal = 1 ]]; then
 
-      if [[ $status_epop_m = "Activo" ]]; then
+      if [[ $status_epop_m = "  Activo" ]]; then
          echo "  1)" Apagar
          if [[ $start_epop = true ]]; then
             echo "  2)" Desactivar AutoInicio
@@ -153,7 +153,7 @@ while [[ $x = x ]]; do
 
       else
 
-         if [[ $status_epop_m = "Inactivo" ]]; then
+         if [[ $status_epop_m = "InActivo" ]]; then
             echo "  1)" Encender
             if [[ $start_epop = false ]]; then
                echo "  2)" Activar AutoInicio
@@ -194,7 +194,7 @@ while [[ $x = x ]]; do
    #Hist
    if [[ $menu_principal = 2 ]]; then
 
-      if [[ $status_hist_m = "Activo" ]]; then
+      if [[ $status_hist_m = "  Activo" ]]; then
          echo " 1)" Apagar
          if [[ $start_hist = true ]]; then
             echo " 2)" Desactivar AutoInicio
@@ -231,7 +231,7 @@ while [[ $x = x ]]; do
 
       else
 
-         if [[ $status_hist_m = "Inactivo" ]]; then
+         if [[ $status_hist_m = "InActivo" ]]; then
             echo " 1)" Encender
             if [[ $start_hist = false ]]; then
                echo " 2)" Activar AutoInicio
@@ -272,7 +272,7 @@ while [[ $x = x ]]; do
    #ACT
    if [[ $menu_principal = 3 ]]; then
 
-      if [[ $status_act_m = "Activo" ]]; then
+      if [[ $status_act_m = "  Activo" ]]; then
          echo " 1)" Apagar
          if [[ $start_act = true ]]; then
             echo " 2)" Desactivar AutoInicio
@@ -309,7 +309,7 @@ while [[ $x = x ]]; do
 
       else
 
-         if [[ $status_act_m = "Inactivo" ]]; then
+         if [[ $status_act_m = "InActivo" ]]; then
             echo " 1)" Encender
             if [[ $start_act = false ]]; then
                echo " 2)" Activar AutoInicio
--- servicios/act.sh
+++ servicios/act.sh
@@ -3,7 +3,7 @@
 #Variables
 Dir_principal=$(cat "$HOME"/.bashrc 2>/dev/null | grep dir_epik00 2>/dev/null | cut -c12- 2>/dev/null)
 x=x
-while [[ $x = "x" ]]; do
+while [ $x = "x" ]; do
     cd /tmp 2>/dev/null || exit
     rm main*.zip 2>/dev/null
     rm -rf ./Epik00-main 2>/dev/null
@@ -11,6 +11,17 @@ while [[ $x = "x" ]]; do
     unzip -qq -o main*.zip 2>/dev/null
     latestversion=$(cat ./Epik00-main/README.md 2>/dev/null | grep "version" 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., " 2>/dev/null)
     version=$(cat "$Dir_principal"/README.md 2>/dev/null | grep "version" 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., ") 2>/dev/null
+    config_newline=$(cat -n "$Dir_principal"/config.txt | tail -1 | awk '{print $1}' )
+    config_line=$(cat -n /tmp/Epik00-main/config.txt | tail -1 | awk '{print $1}' )
+
+    if [ "$config_newline" = "$config_line" ]; then
+    rm /tmp/Epik00-main/config.txt 2>/dev/null
+    else
+    mv /tmp/Epik00-main/config.txt "$Dir_principal"/config.txt
+    export DISPLAY=:0
+    notify-send "Actualizacion de Epik00" "Epik00 ha sido actualizado, (reconfigurelo antes del proximo reinicio" -t 3000
+    fi
+    
     if [ "$latestversion" -gt "$version" ]; then
         cp -ru /tmp/Epik00-main/* "$Dir_principal" 2>/dev/null
         rm -rf ./Epik00-main 2>/dev/null
