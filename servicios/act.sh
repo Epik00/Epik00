69e302bb9df1c398117aaa568dfd57ff78f139a2
Epik00 <114663528+Epik00@users.noreply.github.com>
2022-10-05 23:11:55 +0200

    Epik00

--- servicios/act.sh
+++ servicios/act.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Variables
-Dir_principal=$(cat "$HOME"/.bashrc 2>/dev/null | grep dir_epik00 2>/dev/null | cut -c12- 2>/dev/null)
+Dir_principal=$(grep dir_epik00 "$HOME"/.bashrc 2>/dev/null | cut -c12- 2>/dev/null)
 x=x
 while [ $x = "x" ]; do
     cd /tmp 2>/dev/null || exit
@@ -9,8 +9,8 @@ while [ $x = "x" ]; do
     rm -rf ./Epik00-main 2>/dev/null
     wget https://github.com/Epik00/Epik00/archive/refs/heads/main.zip 2>/dev/null
     unzip -qq -o main*.zip 2>/dev/null
-    latestversion=$(cat ./Epik00-main/README.md 2>/dev/null | grep "version" 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., " 2>/dev/null)
-    version=$(cat "$Dir_principal"/README.md 2>/dev/null | grep "version" 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., ") 2>/dev/null
+    latestversion=$(grep "version" ./Epik00-main/README.md 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., " 2>/dev/null)
+    version=$(grep "version" "$Dir_principal"/README.md 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., ") 2>/dev/null
     config_newline=$(cat -n "$Dir_principal"/config.txt | tail -1 | awk '{print $1}' )
     config_line=$(cat -n /tmp/Epik00-main/config.txt | tail -1 | awk '{print $1}' )
 
