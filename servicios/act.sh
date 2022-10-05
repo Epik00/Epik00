98683b0e583916f94653167d20598da7261e26e5
Epik00 <114663528+Epik00@users.noreply.github.com>
2022-10-05 12:11:40 +0200

    Update act.sh

--- servicios/act.sh
+++ servicios/act.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 #Variables
-Dir_principal=$(cat $HOME/.bashrc | grep dir_epik00| cut -c12-)
+Dir_principal=$(cat "$HOME"/.bashrc | grep dir_epik00 | cut -c12-)
 x=x
 while [[ $x = "x" ]]; do
-cd /tmp || exit
-rm main*.zip; rm -rf ./Epik00-main
-wget https://github.com/Epik00/Epik00/archive/refs/heads/main.zip
-unzip -o main*.zip
-cp -ru /tmp/Epik00-main/* "$Dir_principal"
-rm -rf ./Epik00-main
-rm main*.zip
-
-sleep 1800
+    cd /tmp || exit
+    rm main*.zip
+    rm -rf ./Epik00-main
+    wget https://github.com/Epik00/Epik00/archive/refs/heads/main.zip
+    unzip -o main*.zip
+    cd Epik00-main || exit
+    latestversion=$(cat ./README.md | grep "version" | awk '{print $3}' | tr -d "., ")
+    version=$(cat "$Dir_principal"/README.md | grep "version" | awk '{print $3}' | tr -d "., ")
+    if [ "$latestversion" -gt "$version" ]; then
+        cp -ru /tmp/Epik00-main/* "$Dir_principal"
+        rm -rf ./Epik00-main
+        rm main*.zip
+        sleep 1800
+    else
+        rm -rf ./Epik00-main
+        rm main*.zip
+        sleep 1800
+    fi
 done
