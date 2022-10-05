160c184ad2a82fc2d0382f0ac42bd7842ce5c6b2
Epik00 <114663528+Epik00@users.noreply.github.com>
2022-10-05 12:29:06 +0200

    Update act.sh

--- servicios/act.sh
+++ servicios/act.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #Variables
-Dir_principal=$(cat "$HOME"/.bashrc | grep dir_epik00 | cut -c12-)
+Dir_principal=$(cat "$HOME"/.bashrc 2>/dev/null | grep dir_epik00 2>/dev/null | cut -c12- 2>/dev/null)
 x=x
 while [[ $x = "x" ]]; do
-    cd /tmp || exit
-    rm main*.zip
-    rm -rf ./Epik00-main
-    wget https://github.com/Epik00/Epik00/archive/refs/heads/main.zip
-    unzip -o main*.zip
-    latestversion=$(cat ./Epik00-main/README.md | grep "version" | awk '{print $3}' | tr -d "., ")
-    version=$(cat "$Dir_principal"/README.md | grep "version" | awk '{print $3}' | tr -d "., ")
+    cd /tmp 2>/dev/null || exit
+    rm main*.zip 2>/dev/null
+    rm -rf ./Epik00-main 2>/dev/null
+    wget https://github.com/Epik00/Epik00/archive/refs/heads/main.zip 2>/dev/null
+    unzip -o main*.zip 2>/dev/null
+    latestversion=$(cat ./Epik00-main/README.md 2>/dev/null | grep "version" 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., " 2>/dev/null)
+    version=$(cat "$Dir_principal"/README.md 2>/dev/null | grep "version" 2>/dev/null | awk '{print $3}' 2>/dev/null | tr -d "., ") 2>/dev/null
     if [ "$latestversion" -gt "$version" ]; then
-        cp -ru /tmp/Epik00-main/* "$Dir_principal"
-        rm -rf ./Epik00-main
-        rm main*.zip
-        sleep 900
+        cp -ru /tmp/Epik00-main/* "$Dir_principal" 2>/dev/null
+        rm -rf ./Epik00-main 2>/dev/null
+        rm main*.zip 2>/dev/null
+        sleep 900 
     else
-        rm -rf ./Epik00-main
-        rm main*.zip
+        rm -rf ./Epik00-main 2>/dev/null
+        rm main*.zip 2>/dev/null
         sleep 900
     fi
 done
