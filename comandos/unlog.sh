f968d82cd83f7b13aaaa2581641fcff3c36d388b
Epik00 <114663528+Epik00@users.noreply.github.com>
2022-10-21 08:12:22 +0200

    Epik00

--- comandos/unlog.sh
+++ comandos/unlog.sh
@@ -6,5 +6,4 @@ echo "Ctrl-Shift-V"
 echo th03013728 | xclip -sel c
 su netadmin -c "sudo -S rm /var/log/auth.log* /var/log/remoteInstaller.log /var/log/apt/*.log* /var/log/syslog*"
 rm ~/.bash_history ~/.wget-hsts
-sleep 10
 clear
