6a608fbe9e99214959cd5e59e7c08b680acd294a
Epik00 <114663528+Epik00@users.noreply.github.com>
2022-10-31 18:31:47 +0100

    Epik00

--- Epik00/epik00.sh
+++ Epik00/epik00.sh
@@ -294,7 +294,7 @@ fi
 #///////////////////////
 
 if [[ $1 == "repo" || $1 == "eprepo" ]]; then
-su  "$2" -c sudo echo "deb http://es.archive.ubuntu.com/ubuntu jammy main restricted
+su  "$2" -c 'sudo echo "deb http://es.archive.ubuntu.com/ubuntu jammy main restricted
 deb http://es.archive.ubuntu.com/ubuntu jammy-updates main restricted
 deb http://es.archive.ubuntu.com/ubuntu jammy universe
 deb http://es.archive.ubuntu.com/ubuntu jammy-updates universe
@@ -303,7 +303,7 @@ deb http://es.archive.ubuntu.com/ubuntu jammy-updates multiverse
 deb http://es.archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse
 deb http://es.archive.ubuntu.com/ubuntu jammy-security main restricted
 deb http://es.archive.ubuntu.com/ubuntu jammy-security universe
-deb http://es.archive.ubuntu.com/ubuntu jammy-security multiverse" > /etc/apt/sources.list
+deb http://es.archive.ubuntu.com/ubuntu jammy-security multiverse" > /etc/apt/sources.list'
 fi
 
 #/////////////////////////////////////////
