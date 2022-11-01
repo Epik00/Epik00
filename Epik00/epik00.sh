38f30e10990683cc0516217f871f3e8d0e665576
Epik00 <114663528+Epik00@users.noreply.github.com>
2022-11-01 21:09:27 +0100

    Epik00

--- Epik00/epik00.sh
+++ Epik00/epik00.sh
@@ -10,6 +10,28 @@ rcFile=$configFolder/plasmarc
 normal='\033[0m'
 color='\033[0;92m' 
 
+#/////////////////////
+# Si no hay argumentos
+#/////////////////////
+
+if [[ -z $1 ]]; then
+
+clear
+echo -e "$color"
+echo  "Epik00 😎"
+echo -e "$normal"
+echo "Epik00 es un conjunto de comandos y servicios que te permiten estar mas tranquilo en clase y"
+echo "ademas incluye herramientas y utilidades desde proteccion por contraseña de tu terminal hasta ssh automatizado"
+echo 
+echo "Si necesitas ayuda mira el manual usando el comando en uno de sus formas (ep, epk, epik00) con uno de sus argumentos de ayuda (-h, --help, help man, manual)"
+echo
+echo
+printf "Enter para salir..."
+read -r -s
+
+fi
+
+
 #//////////////////////////////////////////
 # Si el argumento es historial (alias hist)
 #//////////////////////////////////////////
